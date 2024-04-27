78082df97b5d0ea75ebed8375b307080fec5d3cc
YehudaCollins <ycollins555@gmail.com>
2024-04-28 01:16:37 +0300

    .

--- src/card.jsx
+++ src/card.jsx
@@ -1,34 +1,3 @@
-// import React from "react";
-// import './card.css';
-
-// function Card({ card }) {
-
-  
-//   const getGlowClass = () => {
-//     switch (card.color) {
-//       case "golden":
-//         return "golden-glow";
-//       case "blue":
-//         return "blue-glow";
-//       case "green":
-//         return "green-glow";
-//       default:
-//         return "";
-//     }
-//   };
-//   return (
-//     <div>
-//     <div className={`mainCard ${getGlowClass()}`}>
-//         <img src={card.imageUrl} alt={card.name} />
-//       </div>
-//       <h1 className="text1">{card.name}</h1>  
-//     </div>
-//   );
-// }
-// export default Card;
-
-   
-
 import React from "react";
 import { Link } from "react-router-dom";
 import './card.css';
--- src/cardDeta.jsx
+++ src/cardDeta.jsx
@@ -1,19 +1,16 @@
 const cards =[
     {
       "name": "Math",
-      "genres": ["Sports", "Drama"],
       "imageUrl": "/Photos/2.jpeg",
       "color" : "blue"
     }, 
     {
       "name": "English",
-      "genres": ["Sports", "Drama"],
       "imageUrl": "/Photos/11.jpeg",
       "color" : "green"
     },
     {
       "name": "shapes",
-      "genres": ["Sports", "Drama"],
       "imageUrl": "/Photos/3.jpeg",
       "color" : "golden"
     },
