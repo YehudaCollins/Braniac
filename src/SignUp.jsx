d69f421c457a55c189d060fb258621724f71f4e7
YehudaCollins <ycollins555@gmail.com>
2024-05-01 00:06:38 +0300

    .

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,103 +1,141 @@
 import React, { useState } from 'react';
 
-function SignUp(){
-    const [showLogin, setShowLogin] = useState(true);
-  
-    const toggleForm = () => {
-      setShowLogin(!showLogin);
-    };
 
-    const backgroundImageStyle = {
-      backgroundImage: `url("https://static.vecteezy.com/system/resources/thumbnails/027/875/773/original/dark-futuristic-low-poly-surface-background-with-the-gentle-motion-of-black-polygonal-triangle-shapes-and-glowing-red-neon-light-4k-and-looping-technology-motion-background-animation-video.jpg")`,    };
-  
-    return (
-      <div className="main-SignUp" style={backgroundImageStyle}>
-          <div className="titel">
-            <div className="text">Brainiac</div>
-          </div>
-          {showLogin ? (
-          <div>
-            <h2 className="textmain">Login</h2>
-            <form className='Login'>
-              <input
-                className='inpet'
-                type="email"
-                name="email"
-                placeholder="Email"
-              />
-              <input
-                className='inpet'
-                type="password"
-                name="password"
-                placeholder="Password"
-              />
-              <button className='buttonlogin'  type="submit">Login</button>
-              <p><button className='buttonlogin' onClick={toggleForm}>Don't have an account? </button></p>
+function SignUp() {
+  const [showLogin, setShowLogin] = useState(true);
+  const [showForm, setShowForm] = useState(false);
 
-            </form>
-          </div>
-        ) : (
-          <div>
-            <h2 className="textmain">SignUp</h2>
-            <form className='Login'>
-              <input
-                className='inpet'
-                type="email"
-                name="email"
-                placeholder="Email"
-              />
-              <input
-                className='inpet'
-                type="text"
-                name="name"
-                placeholder="First Name"
-              />
-              <input
-                className='inpet'
-                type="text"
-                name="lastName"
-                placeholder="Last Name"
-              />
-              <div className='label'>
-                <label>
-                Boy
-                  <input
-                    className='inpet'
-                    type="radio"
-                    name="gender"
-                    value="boy"
-                  />
-                </label>
-                <label>
-                Girl
-                  <input
-                    className='inpet'
-                    type="radio"
-                    name="gender"
-                    value="girl"
-                  />
-                </label>
-              </div>
-              <input
-                className='inpet'
-                type="number"
-                name="age"
-                placeholder="Age"
-              />
-              <input
-                className='inpet'
-                type="password"
-                name="password"
-                placeholder="Password"
+  const handleClick = () => {
+    setShowForm(true);
+  };
 
-              />
-              <button className="buttonsignup"  type="submit">SignUp</button>
-              <p> <button className="buttonsignup" onClick={toggleForm}>Already have an account?</button></p>
+  const toggleForm = () => {
+    setShowLogin(!showLogin);
+  };
 
-            </form>
-          </div>
-        )}
+  const backgroundImageStyle = {
+    backgroundImage: `url("https://static.vecteezy.com/system/resources/thumbnails/027/875/773/original/dark-futuristic-low-poly-surface-background-with-the-gentle-motion-of-black-polygonal-triangle-shapes-and-glowing-red-neon-light-4k-and-looping-technology-motion-background-animation-video.jpg")`,
+  };
+
+  return (
+    <div className="main-SignUp" style={backgroundImageStyle}>
+      <div className="titel">
+        <div className="text">Brainiac</div>
       </div>
-    );
-  }
+
+      {!showForm && (
+      <>
+        <div className="main-Websitedetails">
+          <div className="picters">
+            <img src="/Photos/1.png"  alt="Picture 1" className="picture" />
+            <img src="/Photos/5.png" alt="Picture 2" className="picture" />
+            <img src="/Photos/4.png" alt="Picture 3" className="picture" />
+          </div>
+          <div className="Websitedetails">
+            <h1 className="Websitedetails-text">The Best and Fun Way for Children to Learn</h1>
+            <div className="colors">
+            <h3 className="color1">English</h3>
+            <h3 className="color2">Math</h3>
+            <h3 className="color3">shapes</h3>
+          </div>
+          <p className="Websitedetails-text2">
+            Brainiac is a special website that helps kids learn through fun games. Parents and teachers can make their own exercises, and the website turns them into super cool games that help kids get better at things like English, math, and shapes - all while having a great time playing!
+          </p>
+          <br></br>
+          <h2 className="Websitedetails-text">
+            The site is completely free. All that remains is to start playing and enjoy learning!
+          </h2>
+          </div>
+        </div>
+          <button className="button1" onClick={handleClick}>Enter the game</button>
+      </>
+      )}
+      
+      {showForm && (
+        <div>
+          {showLogin ? (
+            <div>
+              <h2 className="textmain">Login</h2>
+              <form className='Login'>
+                <input
+                  className='inpet'
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                />
+                <input
+                  className='inpet'
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                />
+                <button className='buttonlogin' type="submit">Login</button>
+                <p><button className='buttonlogin' onClick={toggleForm}>Don't have an account? </button></p>
+              </form>
+            </div>
+          ) : (
+            <div>
+              <h2 className="textmain">SignUp</h2>
+              <form className='Login'>
+                <input
+                  className='inpet'
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                />
+                <input
+                  className='inpet'
+                  type="text"
+                  name="name"
+                  placeholder="First Name"
+                />
+                <input
+                  className='inpet'
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
+                />
+                <div className='label'>
+                  <label>
+                    Boy
+                    <input
+                      className='inpet'
+                      type="radio"
+                      name="gender"
+                      value="boy"
+                    />
+                  </label>
+                  <label>
+                    Girl
+                    <input
+                      className='inpet'
+                      type="radio"
+                      name="gender"
+                      value="girl"
+                    />
+                  </label>
+                </div>
+                <input
+                  className='inpet'
+                  type="number"
+                  name="age"
+                  placeholder="Age"
+                />
+                <input
+                  className='inpet'
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                />
+                <button className="buttonsignup" type="submit">SignUp</button>
+                <p><button className="buttonsignup" onClick={toggleForm}>Already have an account?</button></p>
+              </form>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
 export default SignUp;
