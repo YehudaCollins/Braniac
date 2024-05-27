fc8466b54f513194ebab428f23febc009d8d1426
YehudaCollins <ycollins555@ email.com>
2024-05-27 20:32:55 +0300

    .

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import UnityDemo from "./unity/unityDemo";
+import { LevelProvider } from "./unity/LevelContext";  // Ensure correct import path
+
+
+
+
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-// root.render(<UnityDemo/>)
-root.render(<App/>)
-
- 
+// root.render(<App/>)
+root.render(
+    <LevelProvider>
+      <UnityDemo />
+    </LevelProvider>
+  );
