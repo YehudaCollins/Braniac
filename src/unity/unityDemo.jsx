a3d87e8d1f1354d273b79aff41c482b53bc66e8a
hillelChasanov <hillelich@gmail.com>
2024-05-27 20:40:59 +0300

    .

--- src/unity/unityDemo.jsx
+++ src/unity/unityDemo.jsx
@@ -1,11 +1,107 @@
+// import React, { useEffect, useState } from "react";
+// import { Unity, useUnityContext } from "react-unity-webgl";
+// import { useContext } from 'react';
+// import { LevelContext } from './LevelContext';
+
+// function UnityDemo() {
+//   const { levelIndex } = useContext(LevelContext);
+
+//   const { unityProvider, sendMessage, isLoaded } = useUnityContext({
+//     loaderUrl: "./brainiac-unity/Build/brainiac-unity.loader.js",
+//     dataUrl: "./brainiac-unity/Build/brainiac-unity.data",
+//     frameworkUrl: "./brainiac-unity/Build/brainiac-unity.framework.js",
+//     codeUrl: "./brainiac-unity/Build/brainiac-unity.wasm",
+//   });
+
+//   const handleRedClick = () => {
+//     console.log("Red button clicked. Unity loaded:", isLoaded);
+//     if (isLoaded) {
+//       sendMessage("JavascriptHook", "TintRed");
+//     } else {
+//       console.error("Unity is not loaded yet.");
+//     }
+//   };
+
+//   const handleGreenClick = () => {
+//     console.log("Green button clicked. Unity loaded:", isLoaded);
+//     if (isLoaded) {
+//       sendMessage("JavascriptHook", "TintGreen");
+//     } else {
+//       console.error("Unity is not loaded yet.");
+//     }
+//   };
+//   const handleStringClick = () => {
+//     if (isLoaded) {
+//       sendMessage("JavascriptHook", "SetNumber", 56);
+//     }
+//   };
+
+//   const handleNumberClick = () => {
+//     if (isLoaded) {
+//       sendMessage("JavascriptHook", "SetString", "{levelIndex}");
+//       console.log({levelIndex});
+//     }
+//   };
+
+//   return (
+//     <div className="unity-main">
+//       <div className="hhh">
+//         <button
+//           style={{
+//             backgroundColor: "red",
+//             color: "white",
+//           }}
+//           onClick={handleRedClick}
+//         >
+//           RED
+//         </button>
+//         <button
+//           style={{
+//             backgroundColor: "green",
+//             color: "white",
+//           }}
+//           onClick={handleGreenClick}
+//         >
+//           GREEN
+//         </button>
+
+//         <button
+//           style={{
+//             backgroundColor: "green",
+//             color: "white",
+//           }}
+//           onClick={handleNumberClick}
+//         >
+//           Number
+//         </button>
+//         <button
+//           style={{
+//             backgroundColor: "green",
+//             color: "white",
+//           }}
+//           onClick={handleStringClick}
+//         >
+//           String
+//         </button>
+//       </div>
+
+//       <div>
+//         <Unity className="unity" unityProvider={unityProvider} />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default UnityDemo;
+
+
+
 import React, { useEffect, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import { useContext } from 'react';
 import { LevelContext } from './LevelContext';
 
 function UnityDemo() {
-  const { levelIndex } = useContext(LevelContext);
-
   const { unityProvider, sendMessage, isLoaded } = useUnityContext({
     loaderUrl: "./brainiac-unity/Build/brainiac-unity.loader.js",
     dataUrl: "./brainiac-unity/Build/brainiac-unity.data",
@@ -30,16 +126,20 @@ function UnityDemo() {
       console.error("Unity is not loaded yet.");
     }
   };
-  const handleStringClick = () => {
-    if (isLoaded) {
-      sendMessage("JavascriptHook", "SetNumber", 56);
-    }
-  };
 
   const handleNumberClick = () => {
     if (isLoaded) {
-      sendMessage("JavascriptHook", "SetString", "{levelIndex}");
-      console.log({levelIndex});
+      sendMessage("JavascriptHook", "SetNumber", 56);
+    } else {
+      console.error("Unity is not loaded yet.");
+    }
+  };
+
+  const handleStringClick = () => {
+    if (isLoaded) {
+      sendMessage("JavascriptHook", "SetString", "hello");
+    } else {
+      console.error("Unity is not loaded yet.");
     }
   };
 
@@ -64,10 +164,9 @@ function UnityDemo() {
         >
           GREEN
         </button>
-
         <button
           style={{
-            backgroundColor: "green",
+            backgroundColor: "blue",
             color: "white",
           }}
           onClick={handleNumberClick}
@@ -76,7 +175,7 @@ function UnityDemo() {
         </button>
         <button
           style={{
-            backgroundColor: "green",
+            backgroundColor: "purple",
             color: "white",
           }}
           onClick={handleStringClick}
@@ -84,7 +183,6 @@ function UnityDemo() {
           String
         </button>
       </div>
-
       <div>
         <Unity className="unity" unityProvider={unityProvider} />
       </div>
@@ -93,5 +191,4 @@ function UnityDemo() {
 }
 
 export default UnityDemo;
-
 
